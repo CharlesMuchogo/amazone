33081bd12c082ff9fa4421b8eb11232713d90b4f
charles <muchpaul2@gmail.com>
2023-05-10 00:00:33 +0300

    added lazygridview and appbar

--- app/src/main/java/com/example/amazone/MainActivity.kt
+++ app/src/main/java/com/example/amazone/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -14,14 +15,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -31,6 +42,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -54,7 +66,11 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val viewModel =  hiltViewModel<ProductsViewModel>()
                     val context = LocalContext.current
-                    ProductListScreen(viewModel,context)
+                    Column() {
+                        MyAppBar()
+                        ProductListScreen(viewModel,context)
+                    }
+
                 }
             }
         }
@@ -77,11 +93,7 @@ fun ProductListScreen(viewModel: ProductsViewModel, context: Context) {
 
         ApiStatus.SUCCESS -> {
             productState.data?.let { products ->
-                LazyColumn {
-                    items(products.products) { product ->
-                        ProductCard(product = product)
-                    }
-                }
+                LazyGrid(products.products)
 
             }
         }
@@ -96,53 +108,111 @@ fun ProductListScreen(viewModel: ProductsViewModel, context: Context) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyAppBar() {
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+
+
+        TopAppBar(
+            title = {
+                Card(
+                    shape = RoundedCornerShape(8.dp),
+                    modifier = Modifier
+                        .wrapContentSize(Alignment.Center)
+                        .padding(8.dp).height(48.dp)
+
+                ) {
+                    TextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = "",
+                        onValueChange = {},
+                        placeholder = { Text("Search...") },
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Filled.Search,
+                                contentDescription = "Search",
+                            )
+                        }
+                    )
+                }
+
+            },
+        )
+    }
+}
+
 @Composable
 fun ProductCard(product: Product) {
     Card(
         shape = RoundedCornerShape(8.dp),
-
         modifier = Modifier
+            .height(250.dp)
             .fillMaxWidth()
+            .wrapContentSize(Alignment.Center)
             .padding(8.dp)
 
     ) {
-        Column(
-            modifier = Modifier.padding(8.dp)
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+
+        ){
+            Column(
+                modifier = Modifier.padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+
             ) {
                 AsyncImage(
                     model =  product.image,
                     contentDescription = "Product Image",
                     modifier = Modifier
-                        .height(100.dp)
-                        .width(100.dp),
+                        .height(150.dp)
+                        .width(150.dp),
                     contentScale = ContentScale.Crop
                 )
                 Spacer(modifier = Modifier.width(40.dp))
 
-                Column() {
-                    Text(
-                        text = product.name,
-                        style = MaterialTheme.typography.headlineMedium,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Text(
-                        text = product.price.toString(),
-                        style = MaterialTheme.typography.labelMedium,
-                        color = Color.Gray
-                    )
+                Text(
+                    text = product.name,
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = product.price.toString(),
+                    style = MaterialTheme.typography.labelMedium,
+                    color = Color.Gray,
+                    fontWeight = FontWeight.Bold
+                )
 
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(
-                        text = " ${product.quantity} items in stock",
-                        style = MaterialTheme.typography.labelSmall,
-                        color = Color.Gray
-                    )
-                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = " ${product.quantity} items in stock",
+                    style = MaterialTheme.typography.labelSmall,
+                    color = Color.Gray
+                )
+
             }
+        }
 
+    }
+}
+
+@Composable
+fun LazyGrid(products: List<Product>) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+
+    ) {
+        items(products.size) { product ->
+            ProductCard(product = products.get(product))
         }
     }
 }
